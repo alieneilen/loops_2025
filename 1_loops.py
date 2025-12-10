ca900e8575e691ce94e7c518b3fe2447c213a0b2
alieneilen <125494155+alieneilen@users.noreply.github.com>
2025-12-10 18:38:47 +0000

    credit

--- 1_loops.py
+++ 1_loops.py
@@ -1,42 +1,55 @@
-# Example Practice:
-# Given this list of fruits:
-fruits = ["apple", "banana", "cherry", "date"]
-print (len(fruits))
+# # Example Practice:
+# # Given this list of fruits:
+# fruits = ["apple", "banana", "cherry", "date"]
+# print (len(fruits))
 
-# Challenge:
-# Use a for loop to print each fruit on a new line.
-print(fruits[0])
-print(fruits[1])
-print(fruits[2])
-for fruit in fruits:
-    print(fruit)    
-# i just worked with loops
+# # Challenge:
+# # Use a for loop to print each fruit on a new line.
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+# for fruit in fruits:
+#     print(fruit)    
+# # i just worked with loops
 
-# Given a list of school subjects:
-subjects = ["Math", "Science", "History", "Art"]
-for subject in subjects:
-    print(subject)
-for subject in subjects:
-    if subject == "History":
-        break #stops the list from printing 
-    print(subject)
+# # Given a list of school subjects:
+# subjects = ["Math", "Science", "History", "Art"]
+# for subject in subjects:
+#     print(subject)
+# for subject in subjects:
+#     if subject == "History":
+#         break #stops the list from printing 
+#     print(subject)
 
-for subject in subjects: 
-    if subject == "Science":
-        continue #skips over science but continues with the rest of the list
-    print (subject)
+# for subject in subjects: 
+#     if subject == "Science":
+#         continue #skips over science but continues with the rest of the list
+#     print (subject)
 
-list1000 = list(range(1,1001))
-for num in list1000: 
-    if num > 599: 
-        break
-    print (num)
+# list1000 = list(range(1,1001))
+# for num in list1000: 
+#     if num > 599: 
+#         break
+#     print (num)
 
-for num in list1000:
-    if num > 300 and num < 500:
-        continue #skips numbers between 300 and 500
-    print (num)
+# for num in list1000:
+#     if num > 300 and num < 500:
+#         continue #skips numbers between 300 and 500
+#     print (num)
 
+#----------------------------------------------------------------------------------------------------------
+
+applicants_for_credit = ["Alice", "Bob", "Charlie", "David", "Eve"]
+credit_score = [720, 680, 590, 618, 750]
+
+for applicant, score in zip(applicants_for_credit, credit_score):
+    if score < 600:
+            continue
+    print (applicant + " approved for credit with score: " +str (score))
+
+    
+# zip in for loop if credit score below 600 skip over them and print the rest
+#___________________________________________________________________________________________________________
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
